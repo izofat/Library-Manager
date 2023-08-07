e67b4dc69d818cae2342cb911bdae3887ef8188c
izofat <139143704+izofat@users.noreply.github.com>
2023-08-07 15:07:54 +0300

    Update Customer.cs

--- Customer.cs
+++ Customer.cs
@@ -8,13 +8,14 @@ namespace Library_Manager
 {
     internal class Customer 
     {
-        string connectionString = "Data Source=izofat\\SQLEXPRESS;Initial Catalog=Library;Integrated Security=True";
-        string updateString = "UPDATE BookInfo SET Status = @status WHERE ID = @id";
+        string connectionString = "Data Source=izofat\\SQLEXPRESS;Initial Catalog=Library;Integrated Security=True";        //write the sql server path in here
+        string updateString = "UPDATE BookInfo SET Status = @status WHERE ID = @id";        // write the table name you working on 
         private int row;
         public int Row { get => row; set => row = value; }
+        // barrow the book
         public void BarrowBook()
         {
-            using (SqlConnection connection = new SqlConnection(connectionString))
+            using (SqlConnection connection = new SqlConnection(connectionString))            // this changes the status "Taken"
             {
                 connection.Open();
                 using (SqlCommand updatecmd = new SqlCommand(updateString, connection))
@@ -27,12 +28,13 @@ namespace Library_Manager
 
             }
         }
+        // return the book
         public void ReturnBook ()
         {
             using (SqlConnection connection = new SqlConnection(connectionString))
             {
                 connection.Open();                
-                using (SqlCommand updatecmd = new SqlCommand(updateString, connection))
+                using (SqlCommand updatecmd = new SqlCommand(updateString, connection))    // this changes the status "Available"
                 {
                     updatecmd.Parameters.AddWithValue("@status", "Available");
                     updatecmd.Parameters.AddWithValue("@id", row.ToString());
